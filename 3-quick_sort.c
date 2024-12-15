347e26bafb6029ef9014956d33133d3d1b12ea0c
andreamarquez <andreamarquezg.95@gmail.com>
2024-12-15 15:57:52 +0100

    quick_sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -39,16 +39,22 @@ int partition(int *array, int low, int high, size_t size)
 		if (array[j] < pivot)
 		{
 			i++;
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-			print_array_b(array, size);
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array_b(array, size);
+			}
 		}
 	}
-	temp = array[i + 1];
-	array[i + 1] = array[high];
-	array[high] = temp;
-	print_array_b(array, size);
+	if (i + 1 != high)
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array_b(array, size);
+	}
 
 	return (i + 1);
 }
