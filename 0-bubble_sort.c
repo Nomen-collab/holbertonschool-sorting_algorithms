049f9c8cc0b8148de693fc56366b0d717e269f02
Nomen-collab <nomenaratsimba01@gmail.com>
2024-12-15 01:12:09 +0000

    exo0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,33 +1,30 @@
 #include "sort.h"
 
-
 /**
- * bubble_sort - sorts an array of integers in ascending order using the
- * Bubble sort algorithm
+ * bubble_sort - Sorts an array of integers in ascending order using Bubble sort
  *
- * @array: data to sort
- * @size: size of data
- *
- * Return: No Return
+ * @array: The array to sort
+ * @size: The size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, tmp;
-
-	if (size < 2)
+	size_t i, j;
+	int temp;
+	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i <= size - 1; i++)
+	for (i = 0; i < size - 1; ++i)
 	{
-		for (j = 0; j <= size - 2; j++)
+		for (j = 0; j < size - i - 1; ++j)
 		{
 			if (array[j] > array[j + 1])
 			{
-				tmp = array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
 	}
 }
+
