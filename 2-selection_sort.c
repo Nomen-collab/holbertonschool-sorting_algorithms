5f58a7683647b9061238abe5fbf92c7a4c3628ae
andreamarquez <andreamarquezg.95@gmail.com>
2024-12-15 15:46:47 +0100

    use local print_array_b for selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,4 +1,23 @@
-#include "sort.h"
+#include <stdio.h>
+
+/**
+ * print_array_b - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: The size of the array
+ */
+void print_array_b(const int *array, size_t size)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
 
 /**
  * selection_sort - Sorts an array of integers
@@ -9,29 +28,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t current_index, next_index, min_index;
-    int temp;
+	size_t current_index, next_index, min_index;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (current_index = 0; current_index < size - 1; ++current_index)
-    {
-        /* Find the minimum element in the unsorted part of the array */
-        min_index = current_index;
-        for (next_index = current_index + 1; next_index < size; ++next_index)
-        {
-            if (array[next_index] < array[min_index])
-                min_index = next_index;
-        }
+	for (current_index = 0; current_index < size - 1; ++current_index)
+	{
+		/* Find the minimum element in the unsorted part of the array */
+		min_index = current_index;
+		for (next_index = current_index + 1; next_index < size; ++next_index)
+		{
+			if (array[next_index] < array[min_index])
+				min_index = next_index;
+		}
 
-        /* Swap the found minimum element with the first element */
-        if (min_index != current_index)
-        {
-            temp = array[current_index];
-            array[current_index] = array[min_index];
-            array[min_index] = temp;
-            print_array(array, size);
-        }
-    }
+		/* Swap the found minimum element with the first element */
+		if (min_index != current_index)
+		{
+			temp = array[current_index];
+			array[current_index] = array[min_index];
+			array[min_index] = temp;
+			print_array_b(array, size);
+		}
+	}
 }
