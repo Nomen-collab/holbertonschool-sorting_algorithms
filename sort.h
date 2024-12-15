d8eb808a35de269e6fb5e6a30b290a827ac0d686
Nomen-collab <nomenaratsimba01@gmail.com>
2024-12-15 00:45:41 +0000

    exo 0 correction

--- sort.h
+++ sort.h
@@ -4,33 +4,22 @@
 #include <stddef.h>
 
 /**
- * print_array - Prints an array of integers.
+ * struct listint_s - Doubly linked list node
  *
- * @array: The array to be printed.
- * @size: The number of elements in the array.
- */
-void print_array(const int *array, size_t size);
-
-/**
- * print_list - Prints a list of integers (doubly linked list).
- *
- * @list: The list to be printed.
- */
-void print_list(const listint_t *list);
-
-/**
- * listint_t - Data structure for a doubly linked list node.
- *
- * @n: The integer value stored in the node.
- * @prev: Pointer to the previous element in the list.
- * @next: Pointer to the next element in the list.
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
-		const int n;
-		struct listint_s *prev;
-		struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-#endif /* SORT_H */
+/* Function prototypes */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
+#endif /* SORT_H */
